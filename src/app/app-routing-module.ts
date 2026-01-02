de5c36a3b2b1363ab4a55094a314e3e40ae44291
Alexej Hein <alexej.hein@aol.de>
2026-01-02 10:09:47 +0100

    add home

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './features/home/pages/home-page/home-page';
+import { HomePage } from './features/home/home-page/home-page';
 
 
 const routes: Routes = [
